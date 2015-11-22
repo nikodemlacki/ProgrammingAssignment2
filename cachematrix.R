885eff03deb1fa82f26a9e654664585c10c9a36e
nikodemlacki <nikodem.lacki@gmail.com>
2015-11-22 11:04:12 +0000

    Cache matrix environment function created and tested

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inversedMartix <- NULL
+  set <- function(y) {
+    x <<- y
+    inversedMartix <<- NULL
+  }
+  get <- function() x
+  setInversedMartix <- function(inversedMartixParam) inversedMartix <<- inversedMartixParam
+  getInversedMatrix <- function() inversedMartix
+  list(set = set, get = get,
+       setInversedMartix = setInversedMartix,
+       getInversedMartix = getInversedMatrix)
+  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inversedMartix <- x$getInversedMartix()
+  if(!is.null(inversedMartix)) {
+    message("getting cached inveresed matrix data")
+    return(inversedMartix)
+  }
+  data <- x$get()
+  inversedMartix <- solve(data, ...)
+  x$setInversedMartix(inversedMartix)
+  inversedMartix
 }
